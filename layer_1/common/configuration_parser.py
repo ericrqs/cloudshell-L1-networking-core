71681fedac302ae4ef232c60509d5effd365c648
g8y3e <pava@kyiv.ciklum.net>
2016-05-16 16:52:41 +0300

    added runtime configuration

--- layer_1/common/configuration_parser.py
+++ layer_1/common/configuration_parser.py
@@ -8,18 +8,46 @@ from helper.system_helper import get_file_path
 
 class ConfigurationParser:
     _CONFIG_JSON = None
+    _RUNTIME_CONFIG_JSON = None
     _CONFIG_PATH = "configuration/configuration.json"
+    _ROOT_FOLDER = None
+
+    @staticmethod
+    def _read_json_data(file_path):
+        json_data = open(get_file_path(file_path)).read()
+        return json.loads(json_data)
+
+    @staticmethod
+    def set_root_folder(folder_path):
+        ConfigurationParser._ROOT_FOLDER = folder_path
 
     @staticmethod
     def init():
-        configure_path = ConfigurationParser._CONFIG_PATH
+        if ConfigurationParser._CONFIG_JSON is None:
+            configure_path = ConfigurationParser._CONFIG_PATH
+            ConfigurationParser._CONFIG_JSON = ConfigurationParser._read_json_data(configure_path)
 
-        json_data = open(get_file_path(configure_path)).read()
-        ConfigurationParser._CONFIG_JSON = json.loads(json_data)
+        if ConfigurationParser._RUNTIME_CONFIG_JSON is None:
+            configure_path = ConfigurationParser._ROOT_FOLDER + \
+                             ConfigurationParser._CONFIG_JSON["common_variable"]["runtime_configuration"]
+            ConfigurationParser._RUNTIME_CONFIG_JSON = ConfigurationParser._read_json_data(configure_path)
 
     @staticmethod
     def get(*args):
         ConfigurationParser.init()
+
+        result_data = ConfigurationParser._RUNTIME_CONFIG_JSON
+        founded = True
+        for key in args:
+            if (isinstance(result_data, list) and key < len(result_data)) or \
+                    (isinstance(result_data, dict) and key in result_data):
+                result_data = result_data[key]
+            else:
+                founded = False
+                break
+
+        if founded:
+            return result_data
 
         result_data = ConfigurationParser._CONFIG_JSON
         for key in args:
--- layer_1/main.py
+++ layer_1/main.py
@@ -18,17 +18,6 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         port = int(sys.argv[1])
 
-    request_handler = RequestHandler()
-
-    request_manager = RequestManager()
-    request_manager.bind_command('login', (RequestHandler.login, request_handler))
-    request_manager.bind_command('getresourcedescription', (RequestHandler.get_resource_description, request_handler))
-    request_manager.bind_command('setstateid', (RequestHandler.set_state_id, request_handler))
-    request_manager.bind_command('getstateid', (RequestHandler.get_state_id, request_handler))
-    request_manager.bind_command('mapbidi', (RequestHandler.map_bidi, request_handler))
-    request_manager.bind_command('mapclearto', (RequestHandler.map_clear_to, request_handler))
-    request_manager.bind_command('mapclear', (RequestHandler.map_clear, request_handler))
-
     exe_folder_str = sys.argv[0]
     index = exe_folder_str.rfind('\\')
     if index != -1:
@@ -39,6 +28,18 @@ if __name__ == '__main__':
             exe_folder_str = exe_folder_str[:index + 1]
 
     print exe_folder_str
+    ConfigurationParser.set_root_folder(exe_folder_str)
+
+    request_handler = RequestHandler()
+
+    request_manager = RequestManager()
+    request_manager.bind_command('login', (RequestHandler.login, request_handler))
+    request_manager.bind_command('getresourcedescription', (RequestHandler.get_resource_description, request_handler))
+    request_manager.bind_command('setstateid', (RequestHandler.set_state_id, request_handler))
+    request_manager.bind_command('getstateid', (RequestHandler.get_state_id, request_handler))
+    request_manager.bind_command('mapbidi', (RequestHandler.map_bidi, request_handler))
+    request_manager.bind_command('mapclearto', (RequestHandler.map_clear_to, request_handler))
+    request_manager.bind_command('mapclear', (RequestHandler.map_clear, request_handler))
 
     server_connection = ServerConnection(host, port, request_manager, exe_folder_str)
 
